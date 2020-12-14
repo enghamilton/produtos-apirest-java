46209f9d058bbcda283b0085a522c134ddd45170
Hamilton Kamiya <31168921+enghamilton@users.noreply.github.com>
2020-12-14 05:17:18 -0300

    Update MainController.java

--- src/main/java/com/produtos/apirest/controllers/MainController.java
+++ src/main/java/com/produtos/apirest/controllers/MainController.java
@@ -1,4 +1,4 @@
-package com.produtos.apirest.repository;
+package com.produtos.apirest.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
