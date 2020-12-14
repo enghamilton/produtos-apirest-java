ca18839bd09a6d6feef93b6897e5fb8e7c33a4f1
Hamilton Kamiya <31168921+enghamilton@users.noreply.github.com>
2020-12-14 07:35:44 -0300

    Update MainController.java

--- src/main/java/com/produtos/apirest/controllers/MainController.java
+++ src/main/java/com/produtos/apirest/controllers/MainController.java
@@ -2,14 +2,19 @@ package com.produtos.apirest.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
 	
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String index() {
+		return "index";
+	}
 	//@RequestMapping(value = "/main", method = RequestMethod.GET)
 	@RequestMapping("/main")
 	public String redirect() {
-		return "main";
+		return "redirect:main";
 	}	
 	
 	//@RequestMapping(value = "/profile", method = RequestMethod.GET)
