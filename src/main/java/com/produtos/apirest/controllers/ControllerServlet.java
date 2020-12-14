b860c9f17b5fbeb1d31f6271733ac58a0082d003
Hamilton Kamiya <31168921+enghamilton@users.noreply.github.com>
2020-12-14 08:25:18 -0300

    Update ControllerServlet.java

--- src/main/java/com/produtos/apirest/controllers/ControllerServlet.java
+++ src/main/java/com/produtos/apirest/controllers/ControllerServlet.java
@@ -13,17 +13,44 @@ public class ControllerServlet extends HttpServlet {
       message = "Hello World";
    }
 
-   public void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-      
-      // Set response content type
+   @Override
+   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
       response.setContentType("text/html");
+      PrintWriter out=response.getWriter();
 
-      // Actual logic goes here.
-      PrintWriter out = response.getWriter();
-      out.println("<h1>" + message + "</h1>");
+      RequestDispatcher rd=request.getRequestDispatcher("login-success.jsp");
+      rd.forward(request, response);
+
+      /*
+      String name=request.getParameter("name");
+      String password=request.getParameter("password");
+
+      LoginBean bean=new LoginBean();
+      bean.setName(name);
+      bean.setPassword(password);
+      request.setAttribute("bean",bean);
+
+      boolean status=bean.validate();
+
+      if(status){
+       RequestDispatcher rd=request.getRequestDispatcher("login-success.jsp");
+       rd.forward(request, response);
+      }
+      else{
+       RequestDispatcher rd=request.getRequestDispatcher("login-error.jsp");
+       rd.forward(request, response);
+      }
+      */
+
+
    }
 
+   @Override
+   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+      doPost(req, resp);
+   }
+ 
    public void destroy() {
       // do nothing.
    }
